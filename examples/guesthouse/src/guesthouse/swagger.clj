edc49b12a8ae7f30d8583bfea28e55f7b3fae67f
Tommi Reiman <tommi@iki.fi>
2014-03-24 09:13:24 +0200

    extract body- and return parameters

--- examples/guesthouse/src/guesthouse/swagger.clj
+++ examples/guesthouse/src/guesthouse/swagger.clj
@@ -16,14 +16,24 @@
 (defn- generate-nickname [annotated-handler]
   (str (:api annotated-handler) (get-in annotated-handler [:info :source-map :name])))
 
-(defn collect-swagger-info [swagger {{:keys [method path description]} :info api :api :as annotated-handler}]
+(defn body-parameter [body]
+  (if body
+    [{:name (-> body schema/schema-name str .toLowerCase)
+      :description ""
+      :required true
+      :paramType "body"
+      :type (ring-swagger/resolve-model-vars body)}]))
+
+(defn collect-swagger-info [swagger {{:keys [method path description request responses] :as info} :info api :api :as annotated-handler}]
   (swap! swagger
     update-in [api]
     update-in [:routes]
     conj {:method method
           :uri (create-uri path)
           :metadata {:summary description
-                     :nickname (generate-nickname annotated-handler)}})
+                     :return (get responses 200)
+                     :nickname (generate-nickname annotated-handler)
+                     :parameters (body-parameter (:body request))}})
   annotated-handler)
 
 (defnk $api-docs$GET
