7f78b6096d9aae8945fb4989b66f8d0859916a44
Tommi Reiman <tommi@iki.fi>
2014-03-25 23:02:16 +0200

    set meta-data so that it really stays (http://dev.clojure.org/jira/browse/CLJ-130)

--- examples/guesthouse/src/guesthouse/guestbook.clj
+++ examples/guesthouse/src/guesthouse/guestbook.clj
@@ -1,6 +1,5 @@
 (ns guesthouse.guestbook
-  "A set of handlers for adding, viewing, searching for,
-   and deleting guestbook entries"
+  {:doc "A set of handlers for adding, viewing, searching for, and deleting guestbook entries"}
   (:use plumbing.core)
   (:require
    [schema.core :as s]
